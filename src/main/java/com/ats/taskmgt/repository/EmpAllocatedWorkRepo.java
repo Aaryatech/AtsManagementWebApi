ccca096ffeccd981c892d5d9f5417c87ab9ff9ab
Viplav Thoke <viplavthoke@gmail.com>
2018-06-30 19:05:01 +0530

    /Akshay

--- src/main/java/com/ats/taskmgt/repository/EmpAllocatedWorkRepo.java
+++ src/main/java/com/ats/taskmgt/repository/EmpAllocatedWorkRepo.java
@@ -13,7 +13,7 @@ public interface EmpAllocatedWorkRepo extends JpaRepository<EmpAllocatedWorkRepo
 	@Query(value = "\r\n"
 			+ "SELECT t_projects.project_name, t_task.task_id, COALESCE(SUM(task_planned_hrs),0) as task_planned_hrs,"
 			+ " COALESCE(SUM(task_planned_hrs)/8,0) as no_of_days FROM t_projects, t_task WHERE  t_task.end_date"
-			+ " BETWEEN :fromDate AND :toDate AND t_task.developer_id=:empId AND t_task.dev_status!=3 "
+			+ " BETWEEN :fromDate AND :toDate AND t_task.developer_id=:empId AND t_task.dev_status=1 "
 			+ "GROUP BY t_task.project_id\r\n" + "", nativeQuery = true)
 	EmpAllocatedWorkReport findAllocatedEmpWork(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate,
 			@Param("empId") int empId);
@@ -22,12 +22,12 @@ public interface EmpAllocatedWorkRepo extends JpaRepository<EmpAllocatedWorkRepo
 			"SELECT" + 
 			"        t_task.task_id,t_projects.project_name,COALESCE(SUM(task_planned_hrs), 0) as task_planned_hrs, "
 			+ "  COALESCE(SUM(task_planned_hrs)/8,  0) as no_of_days  FROM     t_projects,   t_task  WHERE   t_task.project_id=:projectId "
-			+ "   AND t_task.developer_id=:empId     and t_task.project_id = t_projects.project_id    AND t_task.dev_status!=3  "
+			+ "   AND t_task.developer_id=:empId     and t_task.project_id = t_projects.project_id    AND t_task.dev_status=1  "
 			+ "", nativeQuery = true)
 	EmpAllocatedWorkReport findAllocatedEmpWorkById(@Param("projectId") int projectId, @Param("empId") int empId);
 
 	@Query(value= "SELECT t_task.task_id,t_projects.project_name,COALESCE(SUM(task_planned_hrs), 0) as task_planned_hrs,"
-			+ "COALESCE(SUM(task_planned_hrs)/8,  0) as no_of_days  FROM     t_projects,   t_task  WHERE  t_task.developer_id=:empId     and t_task.project_id = t_projects.project_id    AND t_task.dev_status!=3  "
+			+ "COALESCE(SUM(task_planned_hrs)/8,  0) as no_of_days  FROM     t_projects,   t_task  WHERE  t_task.developer_id=:empId     and t_task.project_id = t_projects.project_id    AND t_task.dev_status=1  "
 			+ "", nativeQuery = true)
 	EmpAllocatedWorkReport findAllocatedEmpWorkById(@Param("empId") int empId);
 
